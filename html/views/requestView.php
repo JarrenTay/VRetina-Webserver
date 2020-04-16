722dc31a51934fd8e95c544236dcfd2d89caeec3
Team 8 <ukycs499team8@gmail.com>
2020-04-16 16:21:45 +0000

    Self construct json string

--- html/views/requestView.php
+++ html/views/requestView.php
@@ -34,7 +34,20 @@
                 $output = str_replace('{{altText}}', $this->model->name, $output);
                 return $output;
             } else if ($this->model->clientType == 'app') {
-                return json_encode($this->model);
+                //return json_encode($this->model);
+                $requestTemplateName = '/var/www/templates/requestJsonTemplate.json';
+                $requestTemplateStream = fopen($requestTemplateName, 'r') or die('Unable to open request template');
+                $requestTemplate = fread($requestTemplateStream, filesize($requestTemplateName));
+
+                $output = str_replace('{{id}}', $this->model->id, $requestTemplate);
+                $output = str_replace('{{name}}', $this->model->name, $output);
+                $output = str_replace('{{xSize}}', $this->model->xSize, $output);
+                $output = str_replace('{{ySize}}', $this->model->ySize, $output);
+                $output = str_replace('{{filetype}}', $this->model->filetype, $output);
+                $output = str_replace('{{official}}', $this->model->official, $output);
+                $output = str_replace('{{uploaded}}', $this->model->uploaded, $output);    
+                $output = str_replace('{{imageData}}', $this->model->image, $output);
+                return $output;
             } else {
                 return 'Bad Request (clientType not specified)';
             }
